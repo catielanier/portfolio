2309b7ccf6e5d5d33e8a7c07205e571099cf5772
Corey Lanier <iam@coreylanier.com>
2018-06-03 16:49:24 -0400

    finally getting portfolio clicks to slide through

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,35 +1,61 @@
 'use strict';
 
+var app = {};
+
+// Compile all the functions to load.
+
+app.init = function () {
+    app.smoothScroll();
+    app.portfolioExampleSlider();
+};
+
 // Smooth Scrolling
 
-// Select all links with hashes
-$('a[href*="#"]')
-// Remove links that don't actually link to anything
-.not('[href="#"]').not('[href="#0"]').click(function (event) {
-    // On-page links
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        // Does a scroll target exist?
-        if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000, function () {
-                // Callback after animation
-                // Must change focus!
-                var $target = $(target);
-                $target.focus();
-                if ($target.is(":focus")) {
-                    // Checking if the target was focused
-                    return false;
-                } else {
-                    $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                    $target.focus(); // Set focus again
-                };
-            });
+app.smoothScroll = function () {
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]').not('[href="#0"]').click(function (event) {
+        // On-page links
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function () {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
         }
-    }
-});
+    });
+};
+
+app.portfolioExampleSlider = function () {
+    $('.portfolio-section__grid__box').on('mouseover', '.portfolio-slider', function () {
+        console.log('hover');
+        $(this).css('transform', 'translateX(-100%)');
+    });
+
+    $('.portfolio-section__grid__box').on('mouseleave', '.portfolio-slider', function () {
+        $(this).css('transform', 'translateX(0)');
+    });
+};
+
+// Run the damn thing
+
+$(app.init);
